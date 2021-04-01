1250515e95c85e6b01c9b8d665c97c4c32c381b0
nguyenvthao710 <xomgiua9999@gmail.com>
2021-04-02 00:01:49 +0700

    DN
    day la comt dau tien

---
+++ UserControl1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FormDN;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        QL_NguoiDung CauHinh = new QL_NguoiDung();
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox1.Text.Trim()))
+            {
+                MessageBox.Show("thong bao", "khong dc de ten tk");
+                this.textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox1.Text.Trim()))
+            {
+                MessageBox.Show("thong bao", "khong dc de ten tk");
+                this.textBox1.Focus();
+                return;
+            }
+
+            int kq = CauHinh.check();
+            if (kq == 0)
+            {
+                config();
+                //dang nhap thanh cong
+            }
+            if (kq == 1)
+            {
+                // cau hinh trong
+            }
+            if (kq == 2)
+            {
+                //CauHinh hinh sai
+            }
+        }
+
+        private void config()
+        {
+            int ressut;
+            ressut = CauHinh.config(textBox1.Text, textBox2.Text);
+            if (ressut == 10)
+            {
+                // khong co tai khoan
+                return;
+            }
+            else if (ressut == 20)
+            {
+                //tai khoan bi khoa
+                return;
+
+            }
+            else
+            {
+                FrmMain frm = new FrmMain();
+                frm.Show();
+            }   
+            
+           
+
+        }
+    }
+}
